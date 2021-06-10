5f9c8329817254b467fa509f5ae54d37c2748a60
Jonagarme <garciajonathan268@gmail.com>
2021-06-09 22:23:12 -0500

    creando page de slides y añadiendo svg

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
+import 'package:disenos/src/pages/slideshow_page.dart';
 import 'package:flutter/material.dart';
 
 //import 'src/retos/cuadrado_animado_page.dart';
 //import 'package:disenos/src/pages/animaciones_page.dart';
 //import 'package:disenos/src/pages/headers_page.dart';
 //import 'package:disenos/src/labs/circular_progress_page.dart';
-
-import 'package:disenos/src/pages/graficas_circulares_page.dart';
+//import 'package:disenos/src/pages/graficas_circulares_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños App',
-      home: GraficasCirculares(),
+      home: SlideShowPage(),
     );
   }
 }
